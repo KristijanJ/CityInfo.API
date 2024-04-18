1b1167c95fab7a689ccef33eb13184c8c41a5189
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2024-04-18 14:42:03 +0200

    documentation

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -42,7 +42,17 @@ public class CitiesController : ControllerBase
     return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
   }
 
+  /// <summary>
+  /// Get a city by id
+  /// </summary>
+  /// <param name="id">The id of the city to get</param>
+  /// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
+  /// <returns>A city with or without points of interest</returns>
+  /// <response code="200">Returns the requested city</response>
+  /// <response code="404">The requested city was not found</response>
   [HttpGet("{id}")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
   public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
   {
     var cityEntity = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
