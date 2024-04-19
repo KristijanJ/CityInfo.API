9d2b818f550956d5e30b86e56473adaa0288d318
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2024-04-19 14:06:16 +0200

    Forwarding options when hosting with proxy (load balancer)

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Asp.Versioning.ApiExplorer;
 using CityInfo.API;
 using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -137,12 +138,21 @@ builder.Services.AddSwaggerGen(setupAction =>
   });
 });
 
+// Forwarding options when hosting with proxy (load balancer)
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+  options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
   app.UseExceptionHandler();
 }
+
+// Forwarding options when hosting with proxy (load balancer)
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
